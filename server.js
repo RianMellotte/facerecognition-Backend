1e4abcc92b93cb95254c4cccd1282f262edc53cd
Rian Mellotte <Alfred@MacBook-Pro-2.local>
2020-09-06 18:26:10 +0100

    adding port

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.put('/image', (req, res) => { { image.loadImage(req, res, db) }})
 
 app.post('/imageurl', (req, res) => { { image.handleApiCall(req, res) }})
 
-app.listen(3000, () => {
-	console.log('working');
+app.listen(process.env.PORT || 3000, () => {
+	console.log('wapp is running on port ${process.env.PORT}');
 })
 
 
